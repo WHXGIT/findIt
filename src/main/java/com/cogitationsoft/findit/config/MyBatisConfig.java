ba18797ecac5ebf17ab5bda9c92a2df5a9ba64eb
Andy <wanghaoxin5277@gmail.com>
2018-05-07 17:40:07 +0800

    修改配置文件方式

--- src/main/java/com/cogitationsoft/findit/config/MyBatisConfig.java
+++ src/main/java/com/cogitationsoft/findit/config/MyBatisConfig.java
@@ -23,73 +23,45 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @Configuration
 @EnableTransactionManagement
-@PropertySource("classpath:config/druid.properties")
 @MapperScan("com.cogitationsoft.findit.mapper")
 public class MyBatisConfig {
 	@Autowired(required = false)
 	public Environment env;
 
-	@Value("${druid.driverClassName}")
-	private String driver;
-
-	@Value("${druid.url}")
-	private String url;
-
-	@Value("${druid.username}")
-	private String username;
-
-	@Value("${druid.password}")
-	private String password;
-
-	@Value("${druid.initialSize}")
-	private int initialSize;
-
-	@Value("${druid.minIdle}")
-	private int minIdle;
-
-	@Value("${druid.maxActive}")
-	private int maxActive;
-
-	@Value("${druid.maxWait}")
-	private int maxWait;
-
-	@Value("${druid.timeBetweenEvictionRunsMillis}")
-	private int timeBetweenEvictionRunsMillis;
-
-	@Value("${druid.minEvictableIdleTimeMillis}")
-	private int minEvictableIdleTimeMillis;
+	@Autowired
+	private DruidConfig druid;
 
 	@Bean(initMethod = "init", destroyMethod = "close")
 	public DruidDataSource dataSource() {
 		DruidDataSource dataSource = new DruidDataSource();
 		// 基本属性 url、user、password
-		dataSource.setDriverClassName(driver);
-		dataSource.setUrl(url);
-		dataSource.setUsername(username);
-		dataSource.setPassword(password);
+		dataSource.setDriverClassName(druid.getDriver());
+		dataSource.setUrl(druid.getUrl());
+		dataSource.setUsername(druid.getUsername());
+		dataSource.setPassword(druid.getPassword());
 
 		// 配置初始化大小、最小、最大
-		dataSource.setInitialSize(initialSize);
-		dataSource.setMinIdle(minIdle);
-		dataSource.setMaxActive(maxActive);
+		dataSource.setInitialSize(druid.getInitialSize());
+		dataSource.setMinIdle(druid.getMinIdle());
+		dataSource.setMaxActive(druid.getMaxActive());
 
 		// 配置获取连接等待超时的时间
-		dataSource.setMaxWait(maxWait);
+		dataSource.setMaxWait(druid.getMaxWait());
 
 		// 配置间隔多久才进行一次检测，检测需要关闭的空闲连接，单位是毫秒
-		dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+		dataSource.setTimeBetweenEvictionRunsMillis(druid.getTimeBetweenEvictionRunsMillis());
 
 		// 配置一个连接在池中最小生存的时间，单位是毫秒
-		dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+		dataSource.setMinEvictableIdleTimeMillis(druid.getMinEvictableIdleTimeMillis());
 
-		dataSource.setValidationQuery("SELECT 'X'");
-		dataSource.setTestWhileIdle(true);
-		dataSource.setTestOnBorrow(false);
-		dataSource.setTestOnReturn(false);
+		dataSource.setValidationQuery(druid.getValidationQuery());
+		dataSource.setTestWhileIdle(druid.isTestWhileIdle());
+		dataSource.setTestOnBorrow(druid.isTestOnBorrow());
+		dataSource.setTestOnReturn(druid.isTestOnReturn());
 
 		// 是否打开PSCache，并且指定每个连接上PSCache的大小
-		dataSource.setPoolPreparedStatements(false);
-		dataSource.setMaxPoolPreparedStatementPerConnectionSize(20);
+		dataSource.setPoolPreparedStatements(druid.isPoolPreparedStatements());
+		dataSource.setMaxPoolPreparedStatementPerConnectionSize(druid.getMaxPoolPreparedStatementPerConnectionSize());
 		return dataSource;
 	}
 
